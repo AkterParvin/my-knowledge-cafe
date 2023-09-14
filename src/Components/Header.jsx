8a80558532c220dbb9853cf2c1134ca309c08cb8
connect-Parvin <parvin.akter0190@gmail.com>
2023-09-14 14:14:08 +0600

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl  font-bold text-[#111]'>My Knowledge Cafe</h1>
+      
+        </div>
+    );
+};
+
+export default Header;
